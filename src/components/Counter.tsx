1c40c3b19ae4f2fd17bdbdffb87beb01582f7b65
lorangeve <lorangeve@gmail.com>
2026-04-17 19:36:08 +0800

    测试 globalState

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -11,13 +11,17 @@ export default function Counter() {
 
   const count = () => store.count ?? 0;
   const countIns = () => (store.count === null ? 0 : store.count + 1);
+  const countDes = () => (store.count === null ? 0 : store.count - 1);
 
   return (
-    <button
-      class="w-50 rounded-full bg-gray-100 border-2 border-gray-300 focus:border-gray-400 active:border-gray-400 px-8 py-4"
-      onClick={() => setStore("count", countIns)}
-    >
-      Clicks: {count()}
-    </button>
+    <>
+      <button class="btn" onClick={() => setStore("count", countDes)}>
+        -
+      </button>
+      <label class="px-3 text-center">{count()}</label>
+      <button class="btn" onClick={() => setStore("count", countIns)}>
+        +
+      </button>
+    </>
   );
 }
--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,5 +1,4 @@
 import { A } from "@solidjs/router";
-import Counter from "~/components/Counter.1";
 
 export default function About() {
   return (
